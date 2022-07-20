b19c46a854376a3ad3d73ed76a85fe8762f22adf
Abhay Chandel <97330477+AbhayChandel13@users.noreply.github.com>
2022-07-21 00:57:08 +0530

    20july,2022

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 mongoose.connect("mongodb://localhost:27017/students-api")
 .then (()=>{
-    console.log("conection is succesful");
+    console.log("Connection is succesful");
 }).catch((e)=>{
     console.log("No Connection");
 })
