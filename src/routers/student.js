d20c3473d3fe9196cb3bccb75c77c66dd73b86d7
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-07-22 06:02:14 +0530

    commit 2

--- src/routers/student.js
+++ src/routers/student.js
@@ -51,7 +51,7 @@ router.post("/students", async (req, res) => {
   
   //Read the data of registered Students:
   //comment 1 
-  
+  //comment 1 
   router.get("/students", async (req, res) => {
     try {
       const studentsData = await Student.find();
