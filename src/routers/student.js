a224e740334e8e278dbc2f8b25a7801805ed5d58
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-08-02 20:02:57 +0530

    delete comment

--- src/routers/student.js
+++ src/routers/student.js
@@ -41,9 +41,7 @@ router.post("/students", async (req, res) => {
       res.status(400).send(e);
     }
   });
-  
-//You Do not need express.json() and express.urlencoded()
-  //for GET requests or DELETE Requests. We only need it for 
+ 
 
   //You Do not need express.json() and express.urlencoded()
   //for GET requests or DELETE Requests. We only need it for post and put req.
